aba799af07d83d3ff102abb618953b04a7bf52dd
FroznMine <pesafrre@arcor.de>
2014-12-14 20:42:21 +0100

    Added first menu control structures
    First tries to make inventories to choose from.

---
+++ src/main/java/de/froznmine/lobby/event/InventoryChooseEvent.java
@@ -0,0 +1,17 @@
+package de.froznmine.lobby.event;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+
+public class InventoryChooseEvent implements Listener {
+	@EventHandler
+	/** Handles clicks in lobby inventories
+	 * 
+	 */
+	public static void onInvClick(InventoryClickEvent e) {
+		// if inventory correct
+	}
+}
