ef8c6b0cb831b9a582cb7f93bdc5d97afe0fa4e7
FroznMine <pesafrre@arcor.de>
2014-12-21 22:09:33 +0100

    Added class to display scoreboards.

---
+++ src/main/java/de/froznmine/lobby/scoreboard/Scoreboards.java
@@ -0,0 +1,34 @@
+package de.froznmine.lobby.scoreboard;
+
+import org.bukkit.conversations.Conversation;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scoreboard.Scoreboard;
+
+import de.froznmine.lobby.LobbySystem;
+
+public class Scoreboards {
+	/**
+	 * Displays the scoreboard with the given time for the given amount of
+	 * ticks. After that switching back to previous.
+	 *
+	 * @param name
+	 *            the scoreboard to display
+	 * @param ticks
+	 *            the time in ticks to display it
+	 */
+	public static void displayScoreboard(final Player p, final Scoreboard scoreboard, final int ticks) {
+		final Scoreboard old = p.getScoreboard();
+
+		p.setScoreboard(scoreboard);
+
+		final BukkitRunnable runnable = new BukkitRunnable() {
+			@Override
+			public void run() {
+				p.setScoreboard(old);
+			}
+		};
+
+		runnable.runTaskLater(LobbySystem.PLUGIN, ticks);
+	}
+}
